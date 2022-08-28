db6887b6a940d2fbbc0617d15488833aab8b64f2
kurunuma <nouhailaelarroum99@gmail.com>
2022-08-28 08:01:03 +0100

    My form in React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
+import React, {useState} from "react";
 
 function App() {
+  // const [firstName,lastName,email,subject,message, setFirstname, setLastname,setEmail, setSubject,setMessage] = useState("");
+  const [firstName,setFirstname] = useState("");
+  const [lastName,setLastname] = useState("");
+  const [email,setEmail] = useState("");
+  const [subject,setSubject] = useState("");
+  const [message,setMessage] = useState("");
+  const [setStatus] = useState('all');
   return (
     <div className="App">
       <header className="App-header">
-        <Form />
+        <Form   
+         firstName={firstName}
+         lastName={lastName}
+         email={email}
+         subject={subject}
+         message={message}
+         setFirstname={setFirstname}
+         setLastname={setLastname}
+         setEmail={setEmail}
+         setSubject={setSubject}
+         setMessage={setMessage}
+         setStatus={setStatus}/>
       </header>
   
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,67 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import * as React from "react";
+import React from 'react'; 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
 
-const myForm = () => {
+const  myForm = ({firstName,lastName,email,subject,message, setFirstname, setLastname,setEmail, setSubject,setMessage, setStatus}) =>{
+
+      const handleFirstNameChange = (event) => {
+        setFirstname(event.target.value)
+      }
+      const handleLastNameChange = (event) => { 
+        setLastname(event.target.value)
+     }
+     const handleEmailChange = (event) => {
+      setEmail(event.target.value)
+     }
+     const handleSubjectChange = (event) => {
+      setSubject(event.target.value)
+     }
+     const handleMessageChange = (event) => {
+      setMessage(event.target.value)
+     }
+     const handleSubmit = event => {  
+      alert(`First name : ${firstName} \nLast name : ${lastName} \nEmail: ${email} \nSubject: ${subject} \nMessage :${message}`)
+     }
+    // render() {
     return(
-        <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-  
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>        
+      <Form className="container mt-3 mt-3 " onSubmit={handleSubmit}>
+         <Row className="mb-3">         
+          <Form.Group className="col col-sm-6" controlId="firstName">
+            <Form.Label>First name</Form.Label>
+            <Form.Control type="name" placeholder="your first name" value={firstName} onChange={handleFirstNameChange}/>
+          </Form.Group>
+          <Form.Group className="col col-sm-6" controlId="lastName">
+            <Form.Label>Last name</Form.Label>
+            <Form.Control type="name" placeholder="your last name" valur={lastName} onChange={ handleLastNameChange} />
+          </Form.Group>
+          <Form.Group className="col col-12" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" value={email} onChange={ handleEmailChange} />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="col col-12" controlId="subject">
+            <Form.Label>Subject</Form.Label>
+            <Form.Control type="subject" placeholder="your subject" value={subject} onChange={handleSubjectChange} />
+          </Form.Group>
+          <Form.Group className="col col-12" controlId="formBasicPassword">
+            <Form.Label>Message</Form.Label>
+            <Form.Control as="textarea" rows="5" name="message" placeholder="Message" value={message} onChange={handleMessageChange} />          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="submit">
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+          </Form.Group>
+      </Row>
+    </Form>        
     );
+    // }
 }
 export default myForm;
